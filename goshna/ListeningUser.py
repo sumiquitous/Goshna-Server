d92a8d1783dc4f675f77cc20efddf9730e28c46f
Nick Tardif <nicktardif@gmail.com>
2015-04-08 16:13:24 -0400

    Enabled user to add/remove flights from app

--- goshna/ListeningUser.py
+++ goshna/ListeningUser.py
@@ -2,6 +2,13 @@ from flask import Flask, jsonify, abort, request
 from goshna import *
 from goshna import ApiFunctions
 
+class FlightID:
+    def __init__(self, flight_id):
+        self.flight_id = flight_id
+
+    def to_json(self):
+        return {'flight_id': self.flight_id}
+
 class ListeningUser:
     def __init__(self, user_id, flight_id):
         self.user_id = user_id
@@ -20,21 +27,27 @@ class ListeningUser:
 
         return jsonify({'listening_users': listening_users})
 
-    @app.route('/goshna/api/listening_users', methods=['POST'])
-    def create_listening_user():
-        if not request.json or not 'userid' in request.json or not 'flightid' in request.json:
-            abort(400)
+    @app.route('/goshna/api/flights/adduser/<int:user_id>/<int:flight_id>', methods=['GET'])
+    def add_listening_user(user_id, flight_id):
         
-        user_id = request.json['userid']
-        flight_id = request.json['flightid']
         result = ApiFunctions.post_db("INSERT INTO listening_users VALUES (?, ?)", [user_id, flight_id]);
         inserted_id = c.lastrowid
         print u'Inserted new listening_user at row ' + str(inserted_id)
         return jsonify({'id': str(inserted_id)}), 201
 
-    @app.route('/goshna/api/listening_users/<int:listening_user_id>', methods=['DELETE'])
-    def delete_listening_user(listening_user_id):
+    @app.route('/goshna/api/flights/removeuser/<int:user_id>/<int:flight_id>', methods=['GET'])
+    def remove_listening_user(user_id, flight_id):
+        
         ApiFunctions.post_db("DELETE FROM listening_users WHERE user_id=? and flight_id=?", [user_id, flight_id])
-        print u'Deleted listening_user with ID ' + str(inserted_id)
-        return jsonify({'result': True})
+        print u'Deleted listening_user with ID ' + str(0)
+        return jsonify({'id': str(0)})
 
+    @app.route('/goshna/api/users/getflights/<int:user_id>', methods=['GET'])
+    def get_user_flights(user_id):
+        flight_ids = []
+        results = ApiFunctions.query_db("SELECT * FROM listening_users where user_id=?", [user_id])
+        for row in results:
+            flight_id = FlightID(row['flight_id'])
+            flight_ids.append(flight_id.to_json())
+
+        return jsonify({'flights': flight_ids})
