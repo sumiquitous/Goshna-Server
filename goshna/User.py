ec7dd7960122c8e0f6ac6cf3bc018b09ca2ed56b
Nick Tardif <nicktardif@gmail.com>
2015-04-08 13:38:53 -0400

    Added getUserMessages function

--- goshna/User.py
+++ goshna/User.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, abort, request
 from goshna import *
-from goshna import ApiFunctions
+import ApiFunctions, Message
 
 class User:
     def __init__(self, id):
@@ -33,3 +33,23 @@ class User:
         print u'Deleted user with ID ' + str(inserted_id)
         return jsonify({'result': True})
 
+    @app.route('/goshna/api/user/messages/<int:user_id>', methods=['GET'])
+    def get_user_messages(user_id):
+
+        messages = []
+
+        # Get the flight ids from ListeningUsers
+        flights = ApiFunctions.query_db("SELECT * FROM listening_users WHERE user_id=?", [user_id]);
+
+        for row in flights:
+            flight_id = row['flight_id'];
+
+            # Get all the messages that correspond to this flight_id
+            message_results = ApiFunctions.query_db("SELECT * FROM messages where flight_id=?", [flight_id]);
+
+            # Add all the messages from that flight to our list
+            for row in message_results:
+                message = Message.Message(row['id'], row['text'], row['time'], row['flight_id'])
+                messages.append(message.to_json())
+
+        return jsonify({'messages': messages})
